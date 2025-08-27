5c6f047a186d43a945ec45fedbba577606980f7c
David <dav988da@gmail.com>
2025-08-27 08:40:30 -0500

    Ejercicios practicos del 1 - 30 terminados

---
+++ Ejercicios_practicos/ejercicio_29.cpp
@@ -0,0 +1,35 @@
+//! Escriba un programa que adivine un número A (entre 0 y 100) seleccionado p or el
+//! usuario (el número NO se ingresa al programa), el programa imprimirá en pantalla un número B y
+//! el usuario usará los símb olos `>', `<' y `=' para indicarle al programa si B es mayor, menor o igual
+//! que A. El programa imprimira un nuevo número B, con base en simb olo ingresado p or el usuario, y
+//! rep etira el pro ceso hasta acertar el número seleccionado p or usuario
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int min = 0, max = 100, guess;
+    char response;
+
+    cout << "Piensa en un numero entre 0 y 100. \n" ;
+    cout << "Usa '<' si mi numero es menor al tuyo. \n";
+    cout << "Usa '>' si mi numero es mayor al tuyo. \n" ;
+    cout << "Usa '=' si mi numero es igual al tuyo. \n";
+
+    do {
+        guess = min + (max - min) / 2;
+        cout << "Tu numero es: " << guess << endl;
+        cout << "Es tu numero? (>, <, =): ";
+        cin >> response;
+
+        if (response == '>') {
+            min = guess + 1;
+        } else if (response == '<') {
+            max = guess - 1;
+        }
+    } while (response != '=');
+
+    cout << "Tu numero es: " << guess << endl;
+
+    return 0;
+}
