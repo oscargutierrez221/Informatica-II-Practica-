d7f917047a3e1578599d3bf9a5c2b6ac9350d511
David <dav988da@gmail.com>
2025-08-27 16:40:51 -0500

    Avances

---
+++ Ejercicios_Problemas/problema_7.cpp
@@ -0,0 +1,24 @@
+//! En la serie de Fib onacci, cada número es la suma de los 2 anteriores e inicia con 1 y
+//! 1. Ej: 1, 1, 2, 3, 5, 8, ....
+//! Escriba un programa que reciba un número n y halle la suma de to dos los números pares en la serie
+//! de Fib onacci menores a n
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a = 1, b = 1, c = 0, suma = 0;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+    while (c < n) {
+        c = a + b;
+        a - b;
+        b = c;
+        if (c % 2 == 0 && c < n) {
+            suma += c;
+            cout << c << " ";
+            cout << "\nEl resultado de la suma es: " << suma << endl;
+        }
+    }
+    return 0;
+}
